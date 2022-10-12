03becde37e328cfb36fb6b01d6cc9314d40682e5
Kus Cámara <kcmr@users.noreply.github.com>
2022-10-12 21:29:28 +0200

    remove unneeded checkout

--- Jenkinsfile.scripted.groovy
+++ Jenkinsfile.scripted.groovy
@@ -1,15 +1,5 @@
 node {
   timestamps {
-    stage('Checkout SCM') {
-      checkout([
-        $class: 'GitSCM',
-        branches: [[name: '**']],
-        extensions: [],
-        userRemoteConfigs:
-        [[url: 'https://github.com/kcmr/lab-jenkins-npm.git']]
-      ])
-    }
-
     withDockerContainer('node:18-slim') {
       stage('Install') {
         sh 'npm i'
